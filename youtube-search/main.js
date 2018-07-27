a6e7839743e6d1985f4e0cfcaeaa046b7dacfe53
hoangphung <phungminhhoang174@gmail.com>
2018-07-27 20:17:31 +0700

    2

--- youtube-search/main.js
+++ youtube-search/main.js
@@ -1,24 +1,21 @@
 function chan(event) {
     event.preventDefault()
 }
-
-function Search() {
+var check = 0;
+var nextPage = '';
+function Search(next = '') {
     let a = $("#keyword").val();
     $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + a + "&type=video&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw",
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + a + "&type=video&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw&pageToken=" + next,
         type: "GET",
         data: $("#keyword").serialize(),
         success: function (body) {
-            let step= Math.floor(body.pageInfo.totalResults/body.pageInfo.resultsPerPage)+1;
-            let loop = 1;
-            while (loop <= step) {
-                let first =(loop-1)*body.pageInfo.resultsPerPage;
-                let last = first + body.pageInfo.resultsPerPage ;
-                let i;
-                for ( i=first; i < last; i++) {
-                    let b = body.items[i]
-                    $("#result-list").append(
-                        `
+            check = 1;
+            nextPage = body.nextPageToken;
+            for (i = 0; i < 25; i++) {
+                let b = body.items[i];
+                $("#result-list").append(
+                    `
                         <a class="result col-md-12" href="https://www.youtube.com/watch?v=${b.id.videoId}" target="_blank">
                             <img src="${b.snippet.thumbnails.medium.url}" alt=""/>
                             <div class="video_info">
@@ -28,37 +25,24 @@ function Search() {
                             </div>
                         </a>
                         `
-                    )
-                }
-                if(i==body.pageInfo.totalResults) break;
-                $(document).ready(function(){
-                    $(window).scroll(function(){
-                        if($(window).scrollTop()==$(document).height()-$(window).height()){                     
-                            loop+=1;
-                        }
-                    });
-                });
+                )
             }
         },
         error: function (body) {
 
         }
-    })
+    });
 }
 
-
-// for (let i = 0; i < body.pageInfo.resultsPerPage; i++) {
-//     let b = body.items[i]
-//     $("#result-list").append(
-//         `
-//         <a class="result col-md-12" href="https://www.youtube.com/watch?v=${b.id.videoId}" target="_blank">
-//             <img src="${b.snippet.thumbnails.medium.url}" alt=""/>
-//             <div class="video_info">
-//                 <h2 class="title">${b.snippet.title}</h2>
-//                 <p class="description">${b.snippet.description}</p>
-//                 <span>View>></span>
-//             </div>
-//         </a>
-//         `
-//     )
-// }
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(document).height() - $(window).height() - $(window).scrollTop() < 2) {
+            if (check == 1) Search(nextPage);
+        }
+    });
+});
+$(document).ready(function (){
+    $("#btnSearch").click(function(){
+        $("#result-list").empty();
+    });  
+});
